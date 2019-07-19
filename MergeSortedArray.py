31180674974ce1d6dd293eb4dc5c9a09121595a6
SelinaWu <zwu33@illinois.edu>
2019-07-18 23:31:46 -0500

    Update MergeSortedArray.py
    more memory cost, fast run

--- MergeSortedArray.py
+++ MergeSortedArray.py
@@ -9,18 +9,20 @@ def merge(self, nums1, m, nums2, n):
         
         used_ind = 0
         main_ind = 0
+        used_ind2 = 0
         nums1[-m:] = nums1[:m]
         
-        while nums2:
+        while used_ind2<n:
             if used_ind==m:
                 break
-            if nums1[n+used_ind]<nums2[0]:
+            if nums1[n+used_ind]<nums2[used_ind2]:
                 nums1[main_ind] = nums1[n+used_ind]
                 used_ind += 1    
             else:
-                nums1[main_ind] = nums2.pop(0)
+                nums1[main_ind] = nums2[used_ind2]
+                used_ind2 += 1
             
             main_ind += 1
         
-        if nums2:
-            nums1[-len(nums2):] = nums2        
+        if used_ind2<n:
+            nums1[-len(nums2[used_ind2:]):] = nums2[used_ind2:] 
